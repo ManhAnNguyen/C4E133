20e72d393b229bdb59d9997abaa5438598b89891
ManhAnNguyen <thungantn1999@gmail.com>
2022-12-07 10:50:37 +0700

    chore: update

--- day11/app.js
+++ day11/app.js
@@ -3,13 +3,35 @@
 //2 : cho 1 content
 //3 : appendChild
 
-const button = document.querySelector("#button");
+// const button = document.querySelector("#button");
 
-button.addEventListener("click", () => {
+// button.addEventListener("click", () => {
+//   //1
+//   const h1 = document.createElement("h1");
+//   //2
+//   h1.innerHTML = "Hello World";
+//   //3
+//   document.querySelector("body").appendChild(h1);
+// });
+
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const valueInput = input.value;
+  if (valueInput === "") return alert("Yeu cau nhap du");
+  if (valueInput.length < 5) return alert("Todo fai lon hon 5!");
   //1
-  const h1 = document.createElement("h1");
+  const div = document.createElement("div");
   //2
-  h1.innerHTML = "Hello World";
+  div.classList.add("todo");
+  div.innerHTML = `                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+    <h1>${valueInput}</h1>
+    <h2>X</h2>
+  `;
   //3
-  document.querySelector("body").appendChild(h1);
+  document.querySelector("body").appendChild(div);
+
+  input.value = "";
 });
